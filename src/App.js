7b3cb4f75a6d7801c2c13c07d67d6d480e91ec32
sandrusmb <sandrusmb@gmail.com>
2019-12-29 11:50:56 +0100

    Add peliculas component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./assets/css/App.css";
 
 // Importar componentes
 import MiComponente from "./components/MiComponente";
+import Peliculas from "./components/Peliculas";
 
 function App() {
   return (
     <div>
       <section className="componentes">
         <MiComponente />
+        <Peliculas />
       </section>
     </div>
   );
---
+++ src/components/Peliculas.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+class Peliculas extends React.Component {
+  render() {
+    return <div>Soy el componente Peliculas</div>;
+  }
+}
+
+export default Peliculas;
