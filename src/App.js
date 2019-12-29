9ca8318a1705e1eaa0a9f3beabf9e6e33b8959a8
sandrusmb <sandrusmb@gmail.com>
2019-12-29 13:22:01 +0100

    Add Header component and scss

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./assets/css/App.css";
 // Importar componentes
 import MiComponente from "./components/MiComponente";
 import Peliculas from "./components/Peliculas";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div>
       <section className="componentes">
+        <Header />
         <MiComponente />
         <Peliculas />
       </section>
---
+++ src/components/Header.js
@@ -0,0 +1,51 @@
+import React from "react";
+import logo from "../assets/images/logo192.png";
+import "../assets/css/Header.scss";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <div className="header__center">
+          {/*  LOGO */}
+          <div className="header__center__logo">
+            <img
+              src={logo}
+              className="header__center__logo__icon"
+              alt="Logotipo"
+            />
+            <span className="header__center__logo__brand">
+              <strong>Curso</strong>React
+            </span>
+          </div>
+
+          {/* MENU */}
+          <nav className="header__center__menu">
+            <ul className="header__center__menu__ul">
+              <li>
+                <a href="index.html">Inicio</a>
+              </li>
+              <li>
+                <a href="blog.html">Blog</a>
+              </li>
+              <li>
+                <a href="formulario.html">Formulario</a>
+              </li>
+              <li>
+                <a href="#">Pagina 1</a>
+              </li>
+              <li>
+                <a href="#">Pagina 2</a>
+              </li>
+            </ul>
+          </nav>
+
+          {/* OTRA COSA QUE NO SÉ QUÉ ES */}
+          <div className="clearfix"></div>
+        </div>
+      </header>
+    );
+  }
+}
+
+export default Header;
